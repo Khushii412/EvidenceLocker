9a0e4a250e4b01f58f090be147abaa3f3df522d2
khushi <khushibanerjee548@gmail.com>
2026-09-18 20:58:52 +0530

    fix project environment setup

--- Backend/digital_evidence_locker/digital_evidence_locker/settings.py
+++ Backend/digital_evidence_locker/digital_evidence_locker/settings.py
@@ -22,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a2gtu_7a7y!u(2cp*i!o(z&f#%i$nz8jw^_f9ige6u$_s2(r79'
+#SECRET_KEY = 'django-insecure-a2gtu_7a7y!u(2cp*i!o(z&f#%i$nz8jw^_f9ige6u$_s2(r79'
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
